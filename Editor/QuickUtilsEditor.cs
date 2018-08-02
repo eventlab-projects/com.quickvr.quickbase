e58adcaab86df96690bb4ff9c2089b919c7444eb
Ramon <r.oliva@virtualbodyworks.com>
2018-08-02 09:19:18 +0200

    * Added missing file.

---
+++ Editor/QuickUtilsEditor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using System.IO;
+
+namespace QuickVR
+{
+
+    [InitializeOnLoad]
+    public class QuickUtilsEditor
+    {
+        static QuickUtilsEditor()
+        {
+            QuickPlayerPrefs.OnSetValue += SaveSettingsAsset;
+            EditorApplication.playModeStateChanged += PlayModeChanged;
+        }
+
+        private static void SaveSettingsAsset()
+        {
+            UnityEditor.EditorUtility.SetDirty(QuickPlayerPrefs.GetSettingsAsset());
+        }
+
+        private static void PlayModeChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.EnteredPlayMode)
+            {
+                QuickUtils.OnCloseApplication += StopEditor;
+            }
+        }
+
+        private static void StopEditor()
+        {
+            EditorApplication.isPlaying = false;
+        }
+
+        public static void CreateDataFolder(string relativePath)
+        {
+            string path = Application.dataPath + "/" + relativePath;
+            if (!Directory.Exists(path))
+            {
+                Debug.Log("CREATING PATH = " + path);
+                Directory.CreateDirectory(path);
+                AssetDatabase.Refresh();
+            }
+            else
+            {
+                Debug.Log("DIRECTORY EXISTS!!!" + path);
+            }
+        }
+    }
+
+}
