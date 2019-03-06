cc16fa36458847deb5f0c6f9d2946a970ba25b0d
Ramon <ramon@ramon.com>
2019-03-06 18:28:15 +0100

    * The animation now records also the position and orientation of the root at each frame, so it can account for root displacement and makes the animation independent to the orientation when it is applied to another avatar.

--- QuickAnimationRecorder.cs
+++ QuickAnimationRecorder.cs
@@ -9,6 +9,19 @@ using System.IO;
 namespace QuickVR
 {
 
+    public class QuickAnimationFrame
+    {
+
+        #region PUBLIC ATTRIBUTES
+
+        public Vector3 _rootPos = Vector3.zero;
+        public Quaternion _rootRot = Quaternion.identity;
+        public Dictionary<HumanBodyBones, Quaternion> _boneData = new Dictionary<HumanBodyBones, Quaternion>();
+
+        #endregion
+
+    }
+
     [System.Serializable]
     public class QuickAnimationRecorder : MonoBehaviour
     {
@@ -33,7 +46,7 @@ namespace QuickVR
         protected State _state = State.Idle;
 
         protected Animator _animator = null;
-        protected List<Dictionary<HumanBodyBones, Quaternion>> _animationFrames = new List<Dictionary<HumanBodyBones, Quaternion>>();
+        protected List<QuickAnimationFrame> _animationFrames = new List<QuickAnimationFrame>();
         protected Dictionary<HumanBodyBones, Quaternion> _lastLocalRotations = new Dictionary<HumanBodyBones, Quaternion>();
         protected float _fps = 0.0f;
 
@@ -88,12 +101,40 @@ namespace QuickVR
             parent.AppendChild(node);
         }
 
-        protected Quaternion GetLastLocalRotation(HumanBodyBones boneID)
+        protected virtual Quaternion GetRotationFromNode(XmlElement node)
+        {
+            float x = float.Parse(node.GetAttribute("rotX"));
+            float y = float.Parse(node.GetAttribute("rotY"));
+            float z = float.Parse(node.GetAttribute("rotZ"));
+            float w = float.Parse(node.GetAttribute("rotW"));
+
+            return new Quaternion(x, y, z, w);
+        }
+
+        protected virtual Vector3 GetPositionFromNode(XmlElement node)
+        {
+            float x = float.Parse(node.GetAttribute("posX"));
+            float y = float.Parse(node.GetAttribute("posY"));
+            float z = float.Parse(node.GetAttribute("posZ"));
+
+            return new Vector3(x, y, z);
+        }
+
+        protected virtual Quaternion GetBoneRotationAtFrame(HumanBodyBones b, int frameID)
+        {
+            if (_animationFrames[frameID]._boneData.ContainsKey(b))
+            {
+                return Quaternion.Inverse(_animationFrames[frameID]._rootRot) * transform.rotation * _animationFrames[frameID]._boneData[b];
+            }
+            return _animator.GetBoneTransform(b).rotation;
+        }
+
+        protected virtual Quaternion GetLastLocalRotation(HumanBodyBones boneID)
         {
             return _lastLocalRotations.ContainsKey(boneID) ? _lastLocalRotations[boneID] : Quaternion.identity;
         }
 
-        protected string GetFilePath()
+        protected virtual string GetFilePath()
         {
             return Path.Combine(Application.dataPath, _fileName + ".xml");
         }
@@ -108,17 +149,15 @@ namespace QuickVR
 
             foreach (XmlElement frameNode in document.DocumentElement.GetElementsByTagName("FrameData"))
             {
-                Dictionary<HumanBodyBones, Quaternion> frameData = new Dictionary<HumanBodyBones, Quaternion>();
+                QuickAnimationFrame frameData = new QuickAnimationFrame();
+                XmlElement rootNode = (XmlElement)(frameNode.GetElementsByTagName("RootData")[0]);
+                frameData._rootPos = GetPositionFromNode(rootNode);
+                frameData._rootRot = GetRotationFromNode(rootNode);
+                
                 foreach (XmlElement boneNode in frameNode.GetElementsByTagName("BoneData"))
                 {
                     HumanBodyBones boneID = QuickUtils.ParseEnum<HumanBodyBones>(boneNode.GetAttribute("name"));
-
-                    float x = float.Parse(boneNode.GetAttribute("x"));
-                    float y = float.Parse(boneNode.GetAttribute("y"));
-                    float z = float.Parse(boneNode.GetAttribute("z"));
-                    float w = float.Parse(boneNode.GetAttribute("w"));
-
-                    frameData[boneID] = new Quaternion(x, y, z, w);
+                    frameData._boneData[boneID] = GetRotationFromNode(boneNode);
                 }
                 
                 _animationFrames.Add(frameData);
@@ -132,14 +171,26 @@ namespace QuickVR
         protected virtual IEnumerator CoRecordAnimation()
         {
             XmlDocument document = new XmlDocument();
-            XmlNode rootNode = document.CreateElement("AnimationData");
-            WriteAttribute(document, "fps", ref rootNode, 1.0f / Time.fixedDeltaTime);
-            document.AppendChild(rootNode);
+            XmlNode animationDataNode = document.CreateElement("AnimationData");
+            WriteAttribute(document, "fps", ref animationDataNode, 1.0f / Time.fixedDeltaTime);
+            document.AppendChild(animationDataNode);
             
             while (_state == State.Recording)
             {
                 XmlNode frameNode = document.CreateElement("FrameData");
-                
+                XmlNode rootNode = document.CreateElement("RootData");
+
+                WriteAttribute(document, "posX", ref rootNode, transform.position.x);
+                WriteAttribute(document, "posY", ref rootNode, transform.position.y);
+                WriteAttribute(document, "posZ", ref rootNode, transform.position.z);
+
+                WriteAttribute(document, "rotX", ref rootNode, transform.rotation.x);
+                WriteAttribute(document, "rotY", ref rootNode, transform.rotation.y);
+                WriteAttribute(document, "rotZ", ref rootNode, transform.rotation.z);
+                WriteAttribute(document, "rotW", ref rootNode, transform.rotation.w);
+
+                frameNode.AppendChild(rootNode);
+
                 for (int i = 0; i < (int)HumanBodyBones.LastBone; i++)
                 {
                     HumanBodyBones b = (HumanBodyBones)i;
@@ -149,15 +200,15 @@ namespace QuickVR
                     XmlNode boneNode = document.CreateElement("BoneData");
                     Quaternion rot = tBone.rotation;
                     WriteAttribute(document, "name", ref boneNode, b);
-                    WriteAttribute(document, "x", ref boneNode, rot.x);
-                    WriteAttribute(document, "y", ref boneNode, rot.y);
-                    WriteAttribute(document, "z", ref boneNode, rot.z);
-                    WriteAttribute(document, "w", ref boneNode, rot.w);
+                    WriteAttribute(document, "rotX", ref boneNode, rot.x);
+                    WriteAttribute(document, "rotY", ref boneNode, rot.y);
+                    WriteAttribute(document, "rotZ", ref boneNode, rot.z);
+                    WriteAttribute(document, "rotW", ref boneNode, rot.w);
 
                     _lastLocalRotations[b] = tBone.localRotation;
                     frameNode.AppendChild(boneNode);
                 }
-                rootNode.AppendChild(frameNode);
+                animationDataNode.AppendChild(frameNode);
                 
                 yield return new WaitForFixedUpdate();
             }
@@ -187,8 +238,9 @@ namespace QuickVR
                     else ceil = Mathf.Min(ceil, numFrames - 1);
                     int floor = Mathf.Max(0, ceil - 1);
 
-                    Quaternion initialRotation = _animationFrames[floor].ContainsKey(b)? _animationFrames[floor][b] : tBone.rotation;
-                    Quaternion finalRotation = _animationFrames[ceil].ContainsKey(b)? _animationFrames[ceil][b] : tBone.rotation;
+                    Quaternion initialRotation = GetBoneRotationAtFrame(b, floor);
+                    Quaternion finalRotation = GetBoneRotationAtFrame(b, ceil);
+                    
                     tBone.rotation = Quaternion.Lerp(initialRotation, finalRotation, Mathf.Repeat(value, 1.0f));
                 }
 
