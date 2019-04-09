6af5994b9fba1cc0216f0d495dd28dd7ea35a2dc
Ramon <ramon@ramon.com>
2019-04-09 13:04:38 +0200

    * Added an extension to Shuffle a list.

--- QuickBaseUtils/QuickUtils.cs
+++ QuickBaseUtils/QuickUtils.cs
@@ -504,6 +504,19 @@ namespace QuickVR
             return animator.GetLookAtBone(lookAtBone.Value);
         }
 
+        public static void Shuffle<T>(this IList<T> ts)
+        {
+            var count = ts.Count;
+            var last = count - 1;
+            for (var i = 0; i < last; ++i)
+            {
+                var r = UnityEngine.Random.Range(i, count);
+                var tmp = ts[i];
+                ts[i] = ts[r];
+                ts[r] = tmp;
+            }
+        }
+
         #endregion
 
     }
