8761d84f217ca27d197dcd067e83e79b579878da
Ramon <ramon@ramon.com>
2020-01-23 10:57:54 +0100

    * Fixed calibration bug.

--- QuickBaseTracking/QuickBaseHeadTracking/QuickUnityVR/QuickUnityVR.cs
+++ QuickBaseTracking/QuickBaseHeadTracking/QuickUnityVR/QuickUnityVR.cs
@@ -130,7 +130,8 @@ namespace QuickVR {
             QuickVRNode node = _vrPlayArea.GetVRNode(QuickVRNode.Type.Head);
             if (!node) return;
 
-            Vector3 offset = _animator.GetBoneTransform(HumanBodyBones.Head).position - node.GetTrackedObject().transform.position;
+            //Vector3 offset = _animator.GetBoneTransform(HumanBodyBones.Head).position - node.GetTrackedObject().transform.position;
+            Vector3 offset = _ikManager.GetIKTarget(HumanBodyBones.Head).position - node.GetTrackedObject().transform.position;
             _vrPlayArea.transform.position += offset;
         }
 
