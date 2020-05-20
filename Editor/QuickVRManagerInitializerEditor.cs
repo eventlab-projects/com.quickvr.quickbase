ea3a9000816245402b46d562eae62d83f3574c87
Ramon <a@a.com>
2020-05-20 11:33:59 +0200

    * Forced QuickVRManager to be initialized on load.

---
+++ Editor/QuickVRManagerInitializerEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+#if UNITY_WEBGL
+using WebXR;
+#endif
+
+namespace QuickVR
+{
+
+    [InitializeOnLoad]
+    public class QuickVRManagerInitializerEditor
+    {
+
+        #region CREATION AND DESTRUCTION
+
+        static QuickVRManagerInitializerEditor()
+        {
+            QuickVRManager vrManager = QuickSingletonManager.GetInstance<QuickVRManager>();
+#if UNITY_WEBGL
+            vrManager.GetOrCreateComponent<WebXRManager>();
+#endif
+        }
+
+        #endregion
+
+    }
+
+}
+
--- QuickBaseTracking/QuickBaseHeadTracking/QuickUnityVR/QuickVRCameraController.cs
+++ QuickBaseTracking/QuickBaseHeadTracking/QuickUnityVR/QuickVRCameraController.cs
@@ -63,17 +63,20 @@ namespace QuickVR
             _camera.farClipPlane = _cameraFarPlane;
             _camera.cullingMask = _visibleLayers.value;
 
-            //Apply the correct rotation to the cameracontrollerroot:
-            Vector3 up = animator.transform.up;
-            Vector3 rightCam = Vector3.ProjectOnPlane(_camera.transform.right, up).normalized;
-            Vector3 r = animator.GetBoneTransform(HumanBodyBones.RightEye).position - animator.GetBoneTransform(HumanBodyBones.LeftEye).position;
-            Vector3 rightHead = Vector3.ProjectOnPlane(r, up).normalized;
-            float rotOffset = Vector3.SignedAngle(rightCam, rightHead, up);
-            transform.Rotate(up, rotOffset, Space.World);
+            if (animator)
+            {
+                //Apply the correct rotation to the cameracontrollerroot:
+                Vector3 up = animator.transform.up;
+                Vector3 rightCam = Vector3.ProjectOnPlane(_camera.transform.right, up).normalized;
+                Vector3 r = animator.GetBoneTransform(HumanBodyBones.RightEye).position - animator.GetBoneTransform(HumanBodyBones.LeftEye).position;
+                Vector3 rightHead = Vector3.ProjectOnPlane(r, up).normalized;
+                float rotOffset = Vector3.SignedAngle(rightCam, rightHead, up);
+                transform.Rotate(up, rotOffset, Space.World);
 
-            //This forces the camera to be in the Avatar's eye center. 
-            Vector3 offset = animator.GetEyeCenterPosition() - _camera.transform.position;
-            transform.position += offset;
+                //This forces the camera to be in the Avatar's eye center. 
+                Vector3 offset = animator.GetEyeCenterPosition() - _camera.transform.position;
+                transform.position += offset;
+            }
         }
 
         #endregion
