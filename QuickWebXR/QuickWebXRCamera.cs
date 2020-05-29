b11b59330c22006688302edf90d41ebdf367827e
Ramon <a@a.com>
2020-05-29 10:48:06 +0200

    * Readded QuickWebXR/QuickWebXRCamera.cs

---
+++ QuickWebXR/QuickWebXRCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using WebXR;
+
+namespace QuickVR
+{
+    public class QuickWebXRCamera : WebXRCamera
+    {
+
+        #region CREATION AND DESTRUCTION
+
+        protected virtual void Awake()
+        {
+            Camera camLeft = transform.GetChild(1).GetComponent<Camera>();
+            camLeft.stereoTargetEye = StereoTargetEyeMask.Left;
+            camLeft.tag = "MainCamera";
+
+            transform.GetChild(2).GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.Right;
+        }
+
+        #endregion
+
+    }
+
+}
+
+
