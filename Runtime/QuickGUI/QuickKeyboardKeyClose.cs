364bc3073de3de3bf7775fb8d5b271d15ab8028f
Ramon <a@a.com>
2021-12-20 17:27:07 +0100

    * Extending QuickGUI/QuickKeyboard.cs functionalities.

---
+++ Runtime/QuickGUI/QuickKeyboardKeyClose.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+using TMPro;
+
+namespace QuickVR 
+{
+
+	public class QuickKeyboardKeyClose : QuickKeyboardKey
+	{
+
+		#region GET AND SET
+
+		public override void DoAction()
+		{
+			_keyboard.Enable(false);
+		}
+
+		#endregion
+
+    }
+		
+}
