4d1e21414f921e27e38066afaf5f5e6a8fe533f2
Ramon <ramon.oliva.martinez@gmail.com>
2022-08-26 12:55:47 +0200

    * QuickEnableRenderers now let's you specify a list of the renderers that you want to disable/enable. This is because for example, there are other renderers attached to the avatar that should not be taken into consideration, such as the renderers for the ray interactors, as some times we want to have an invisible body but able to interact with the environment using the rays.

--- Runtime/QuickEnableRenderers.cs
+++ Runtime/QuickEnableRenderers.cs
@@ -5,22 +5,47 @@ using UnityEngine;
 public class QuickEnableRenderers : MonoBehaviour
 {
 
+    #region PUBLIC ATTRIBUTES
+
     public bool _visible = true;
+    public List<Renderer> _renderers = null;
+
+    #endregion
+
+    #region CREATION AND DESTRUCTION
 
     protected virtual void Start()
     {
+        if (_renderers == null)
+        {
+            Reset();
+        }
+
         SetVisible(_visible);
     }
 
+    protected virtual void Reset()
+    {
+        _renderers = new List<Renderer>(GetComponentsInChildren<Renderer>());
+    }
+
+    #endregion
+
+    #region GET AND SET
+
     public virtual void SetVisible(bool visible)
     {
-        Renderer[] renderers = GetComponentsInChildren<Renderer>();
-        foreach (Renderer r in renderers)
+        if (_renderers != null)
         {
-            r.enabled = visible;
+            foreach (Renderer r in _renderers)
+            {
+                r.enabled = visible;
+            }
         }
 
         _visible = visible;
     }
+
+    #endregion
 
 }
