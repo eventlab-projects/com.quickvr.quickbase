ec42616fb4f5a53de91233e2c1b3d96a2c21247f
Ramon <ramon.oliva.martinez@gmail.com>
2022-11-09 16:59:51 +0100

    * Testing Quest Face Tracking feature.

--- Samples/02_InputManager/CharacterController.cs
+++ Samples/02_InputManager/CharacterController.cs
@@ -4,32 +4,38 @@ using UnityEngine;
 
 using QuickVR;
 
-public class CharacterController : MonoBehaviour
+namespace QuickVR.Samples
 {
-
-    public bool _move = true;
-
-    public float _maxLinearSpeed = 3;
-    public float _maxAngularSpeed = 45;
-
-    void Update()
+    public class CharacterController : MonoBehaviour
     {
-        if (InputManager.GetButtonDown("ToggleMove"))
+
+        public bool _move = true;
+
+        public float _maxLinearSpeed = 3;
+        public float _maxAngularSpeed = 45;
+
+        void Update()
         {
-            _move = !_move;
+            if (InputManager.GetButtonDown("ToggleMove"))
+            {
+                _move = !_move;
+            }
+
+            if (_move)
+            {
+                float vValue = InputManager.GetAxis(InputManager.DEFAULT_AXIS_VERTICAL);
+                transform.Translate(Vector3.forward * vValue * _maxLinearSpeed * Time.deltaTime, Space.Self);
+
+                float hValue = InputManager.GetAxis(InputManager.DEFAULT_AXIS_HORIZONTAL);
+                transform.Rotate(Vector3.up, hValue * _maxAngularSpeed * Time.deltaTime, Space.Self);
+            }
         }
 
-        if (_move)
-        {
-            float vValue = InputManager.GetAxis(InputManager.DEFAULT_AXIS_VERTICAL);
-            transform.Translate(Vector3.forward * vValue * _maxLinearSpeed * Time.deltaTime, Space.Self);
-
-            float hValue = InputManager.GetAxis(InputManager.DEFAULT_AXIS_HORIZONTAL);
-            transform.Rotate(Vector3.up, hValue * _maxAngularSpeed * Time.deltaTime, Space.Self);
-        }
     }
 
 }
+
+
 
 
 
