28953f1705779ce3b8767fd4930887d23f342279
Ramon <a@a.com>
2022-12-09 15:31:14 +0100

    * Introducing QuickZipManager

---
+++ Runtime/QuickUtils/QuickZipManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+using System.IO.Compression;
+
+namespace QuickVR
+{
+
+    public static class QuickZipManager
+    {
+        
+        /// <summary>
+        /// Creates a zip file from a single file. 
+        /// </summary>
+        /// <param name="pathSrc"></param>
+        /// <param name="pathDst"></param>
+        public static void CreateFromFile(string pathSrc, string pathDst)
+        {
+            if (IsFilePath(pathSrc))
+            {
+                using (FileStream fs = new FileStream(pathDst, FileMode.Create))
+                using (ZipArchive arch = new ZipArchive(fs, ZipArchiveMode.Create))
+                {
+                    arch.CreateEntryFromFile(pathSrc, Path.GetFileName(pathSrc));
+                }
+            }
+            else
+            {
+                Debug.LogError("pathSrc is not a file path. Use CreateFromDirectory instead. ");
+            }
+        }
+
+        /// <summary>
+        /// Creates a zip file from a directory. 
+        /// </summary>
+        /// <param name="pathSrc"></param>
+        /// <param name="pathDst"></param>
+        public static void CreateFromDirectory(string pathSrc, string pathDst)
+        {
+            if (IsDirectoryPath(pathSrc))
+            {
+                ZipFile.CreateFromDirectory(pathSrc, pathDst);
+            }
+            else
+            {
+                Debug.LogError("pathSrc is not a directory path. Use CreateFromFile instead. ");
+            }
+        }
+
+        private static bool IsFilePath(string path)
+        {
+            return !IsDirectoryPath(path);
+        }
+
+        private static bool IsDirectoryPath(string path)
+        {
+            // get the file attributes for file or directory
+            FileAttributes attr = File.GetAttributes(path);
+
+            //detect whether its a directory or file
+            return (attr & FileAttributes.Directory) == FileAttributes.Directory;
+        }
+
+    }
+
+}
+
+
