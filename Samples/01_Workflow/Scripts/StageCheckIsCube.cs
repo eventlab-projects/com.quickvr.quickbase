cfd8544f9882f25a54531c0b2f7492b16f480ca8
Ramon <a@a.com>
2023-01-18 09:28:44 +0100

    * Working on samples.

---
+++ Samples/01_Workflow/Scripts/StageCheckIsCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QuickVR.Samples.Workflow
+{
+
+    public class StageCheckIsCube : QuickStageCondition
+    {
+
+        public bool _spawnCubes = true;
+
+        protected override bool Condition()
+        {
+            return _spawnCubes;
+        }
+
+    }
+
+}
+
+
+
---
+++ Samples/01_Workflow/Scripts/StageSpawnSphere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QuickVR.Samples.Workflow
+{
+
+    public class StageSpawnSphere : QuickStageBase
+    {
+
+        public Transform _spawnCenter = null;
+        public float _spawnRadius = 1.0f;
+
+        protected override IEnumerator CoUpdate()
+        {
+            yield return new WaitForSeconds(3);
+
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            cube.transform.parent = transform;
+            cube.transform.localScale = Vector3.one * 0.25f;
+
+            cube.transform.position = _spawnCenter.position + (Random.insideUnitSphere * _spawnRadius);
+        }
+
+    }
+
+}
+
+
+
