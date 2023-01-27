e2a3a283c12377fccb8b83efdaf59aabe4b613ba
Ramon <ramon.oliva.martinez@gmail.com>
2023-01-27 21:03:30 +0100

    * Removed QuickWebXR, as we do not currently use it, plus it has a lot of warnings.

--- Runtime/QuickWebXR/WebXR/Scripts/WebXRDisplayCapabilities.cs
+++
@@ -1,8 +0,0 @@
-﻿[System.Serializable]
-public class WebXRDisplayCapabilities
-{
-	public bool supportsInline;
-	public bool supportsImmersiveVR;
-	public bool supportsImmersiveAR;
-	public bool isSecureContext;
-}
